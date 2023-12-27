9d1171d859024f609d5bfe0fc37e38d65a1b49bb
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2023-12-27 13:55:45 +0200

    contacts list has been finished

--- vite.config.js
+++ vite.config.js
@@ -7,58 +7,58 @@ const root = resolve(__dirname, 'src');
 const outDir = resolve(__dirname, 'dist');
 
 export default defineConfig({
-        root,
-        publicDir: "../public",
+    root,
+    publicDir: "../public",
 
-        server: {
-            port: 7777,
-            host: true,
-            // open: "index.html"
-        },
+    server: {
+        port: 7777,
+        host: true,
+        // open: "index.html"
+    },
 
-        preview:{
-            port: 9999,
-            open: "index.html"
-        },
-    
-        plugins:[
-            inspect(),
-            handlebars({
-                partialDirectory: resolve(__dirname, 'src/partials'),
-            }),
-        ],
-    
-        build: {
-            outDir,
-            emptyOutDir: true,
-            minify: false,
-    
-            rollupOptions: {
-                input: {
-                    main: resolve(root, 'index.html'),
-                    admin: resolve(root, 'test.html'),
-                },
-    
-                output:{
-                    chunkFileNames: 'assets/js/[name]-[hash].js',
-                    entryFileNames: 'assets/js/[name]-[hash].js',
-    
-                    assetFileNames: ({name}) => {
-                        if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')){
-                            return 'assets/img/[name]-[hash][extname]';
-                        }
-                        
-                        if (/\.css$/.test(name ?? '')) {
-                            return 'assets/css/[name]-[hash][extname]';   
-                        }
+    preview: {
+        port: 9999,
+        open: "index.html"
+    },
 
-                        if (/\.(ttf|woff)$/.test(name ?? '')){
-                            return 'assets/fonts/[name]-[hash][extname]';
-                        }
-               
-                        return 'assets/[name]-[hash][extname]';
+    plugins: [
+        inspect(),
+        handlebars({
+            partialDirectory: resolve(__dirname, 'src/partials'),
+        }),
+    ],
+
+    build: {
+        outDir,
+        emptyOutDir: true,
+        minify: false,
+
+        rollupOptions: {
+            input: {
+                main: resolve(root, 'index.html'),
+                production: resolve(root, 'production.html')
+            },
+
+            output: {
+                chunkFileNames: 'assets/js/[name]-[hash].js',
+                entryFileNames: 'assets/js/[name]-[hash].js',
+
+                assetFileNames: ({ name }) => {
+                    if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+                        return 'assets/img/[name]-[hash][extname]';
                     }
+
+                    if (/\.css$/.test(name ?? '')) {
+                        return 'assets/css/[name]-[hash][extname]';
+                    }
+
+                    if (/\.(ttf|woff)$/.test(name ?? '')) {
+                        return 'assets/fonts/[name]-[hash][extname]';
+                    }
+
+                    return 'assets/[name]-[hash][extname]';
                 }
             }
         }
-    });
+    }
+});
